5c9500460ca1ff0a5a5aab60b4a478e2ca0aa215
Roland Fredenhagen <dev@modprog.de>
2023-11-13 17:46:57 +0100

    remove skip from derive(LinearInterpolate)

--- gooey-macros/src/animation.rs
+++ gooey-macros/src/animation.rs
@@ -1,15 +1,8 @@
-use attribute_derive::FromAttr;
 use manyhow::bail;
 use quote::ToTokens;
 use syn::{Field, ItemStruct};
 
 use crate::*;
-
-#[derive(FromAttr)]
-#[attribute(ident = interpolate)]
-struct FieldAttributes {
-    skip: bool,
-}
 
 pub fn linear_interpolate(
     ItemStruct {
@@ -28,38 +21,46 @@ pub fn linear_interpolate(
         fields => fields
             .into_iter()
             .enumerate()
-            .map(|(idx, Field { attrs, ident, .. })| {
+            .map(|(idx, Field {  ident, .. })| {
                 let ident = ident
                     .map(ToTokens::into_token_stream)
                     .unwrap_or_else(|| proc_macro2::Literal::usize_unsuffixed(idx).into_token_stream());
-                Ok(if FieldAttributes::from_attributes(&attrs)?.skip {
-                    quote!(#ident: __target.#ident,)
-                } else {
                     quote!(#ident: ::gooey::animation::LinearInterpolate::lerp(&self.#ident, &__target.#ident, __percent),)
-                })
             }),
-    }.collect::<Result>()?;
+    };
 
     Ok(quote! {
         impl ::gooey::animation::LinearInterpolate for #ident {
             fn lerp(&self, __target: &Self, __percent: f32) -> Self {
-                #ident{#fields}
+                #ident{#(#fields)*}
             }
         }
     })
 }
 
+#[cfg(test)]
+macro_rules! expansion_snapshot {
+    (#[derive($fn:expr)]$($tokens:tt)*) => {{
+        use insta::assert_snapshot;
+        use prettyplease::unparse;
+        use syn::{parse2, parse_quote};
+        let input = parse_quote!($($tokens)*);
+        let output = $fn(input).unwrap();
+        assert_snapshot!(unparse(&parse2(output).unwrap()))
+    }};
+}
+
 #[test]
 fn test() {
-    use insta::assert_snapshot;
-    use prettyplease::unparse;
-    use syn::{parse2, parse_quote};
-    let input = parse_quote!(
+    expansion_snapshot! {
+        #[derive(linear_interpolate)]
         struct HelloWorld {
             fielda: Hello,
             fieldb: World,
         }
-    );
-    let output = linear_interpolate(input).unwrap();
-    assert_snapshot!(unparse(&parse2(output).unwrap()))
+    };
+    expansion_snapshot! {
+        #[derive(linear_interpolate)]
+        struct HelloWorld(Hello, World);
+    };
 }
