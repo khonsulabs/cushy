fbabbdbd99fe0a3613aeb7d3fe63515654d42cba
Jonathan Johnson <jon@khonsulabs.com>
2024-07-08 17:07:00 -0700

    Reverting example change
    Was an unrelated test, oops.

--- examples/unsaved-changes.rs
+++ examples/unsaved-changes.rs
@@ -3,16 +3,13 @@ use cushy::{
     widget::MakeWidget,
     Run,
 };
-use figures::{units::UPx, Size};
 
 fn main() -> cushy::Result {
     let has_unsaved_changes = Dynamic::new(true);
-    let inner_size = Dynamic::new(Size::new(UPx::new(1000), UPx::new(800)));
 
     "Prevent Closing"
         .into_checkbox(has_unsaved_changes.clone())
         .into_window()
         .on_close_requested(move |()| !has_unsaved_changes.get())
-        .inner_size(inner_size)
         .run()
 }
