787b398c1f60b16dbe1d2038a7550693370e2512
Dominic Clifton <me@dominicclifton.name>
2024-11-14 17:47:26 +0100

    Create appropriate document when OK is clicked.

--- examples/tabbed-document-ui/src/documents/image.rs
+++ examples/tabbed-document-ui/src/documents/image.rs
@@ -1,3 +1,4 @@
+use std::fs::File;
 use crate::WidgetInstance;
 use std::path::PathBuf;
 use image::ImageReader;
@@ -10,12 +11,28 @@ pub struct ImageDocument {
 }
 
 impl ImageDocument {
-    pub fn from_path(path: PathBuf) -> ImageDocument {
-
+    pub fn from_path(path: PathBuf) -> Self {
         Self {
             path,
         }
     }
+
+    pub fn new(path: PathBuf) -> Self {
+
+        let mut imgbuf = image::ImageBuffer::<image::Rgb<u8>, Vec<u8>>::new(256, 256);
+
+        for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+            let r = (0.3 * x as f32) as u8;
+            let b = (0.3 * y as f32) as u8;
+            *pixel = image::Rgb([r, 0, b]);
+        }
+
+        let mut file = File::create_new(path.clone()).unwrap();
+        imgbuf.write_to(&mut file, image::ImageFormat::Png).expect("should write to file");
+
+        Self::from_path(path)
+    }
+
 
     pub fn create_content(&self) -> WidgetInstance {
         println!("ImageDocument::create_content. path: {:?}", self.path);
