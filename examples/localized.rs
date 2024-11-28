a91e5308cf34a8a6150de6ebfbd366b580708de5
Dominic Clifton <me@dominicclifton.name>
2024-11-28 19:43:19 +0100

    Add a comment regarding missing translation files.

--- examples/localized.rs
+++ examples/localized.rs
@@ -66,6 +66,7 @@ fn localized() -> impl MakeWidget {
         .and(dynamic_container)
         .into_rows()
         .contain()
+        .centered()
 }
 
 #[derive(Default, Eq, PartialEq, Debug, Clone, Copy)]
@@ -91,17 +92,20 @@ fn main() -> cushy::Result {
 
     let mut app = PendingApp::default();
 
-    let en_us_locale: LanguageIdentifier = "en-US".parse().unwrap();
-    let en_gb_locale: LanguageIdentifier = "en-GB".parse().unwrap();
-    let es_es_locale: LanguageIdentifier = "es-ES".parse().unwrap();
+    // If you comment this block out, you can see the effect of having missing translation files.
+    {
+        let en_us_locale: LanguageIdentifier = "en-US".parse().unwrap();
+        let en_gb_locale: LanguageIdentifier = "en-GB".parse().unwrap();
+        let es_es_locale: LanguageIdentifier = "es-ES".parse().unwrap();
 
-    let translations = app.cushy().translations();
-    translations
-        .add(en_us_locale, include_str!("assets/translations/en-US/hello.ftl").to_owned());
-    translations
-        .add(en_gb_locale, include_str!("assets/translations/en-GB/hello.ftl").to_owned());
-    translations
-        .add(es_es_locale, include_str!("assets/translations/es-ES/hello.ftl").to_owned());
+        let translations = app.cushy().translations();
+        translations
+            .add(en_us_locale, include_str!("assets/translations/en-US/hello.ftl").to_owned());
+        translations
+            .add(en_gb_locale, include_str!("assets/translations/en-GB/hello.ftl").to_owned());
+        translations
+            .add(es_es_locale, include_str!("assets/translations/es-ES/hello.ftl").to_owned());
+    }
 
     let _window_handle = localized()
         .into_window()
