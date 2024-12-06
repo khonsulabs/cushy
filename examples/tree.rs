2982e8de09035fe846763e3dddacfa162b26d17f
Dominic Clifton <me@dominicclifton.name>
2024-12-06 16:54:10 +0100

    Tree widget - WIP 12 - Highlight the API inadequacies by adding buttons into the nodes that need to refer to the tree and the node itself.

--- examples/tree.rs
+++ examples/tree.rs
@@ -1,7 +1,38 @@
 use cushy::{App, Open, Run};
 use cushy::widget::MakeWidget;
+use cushy::widgets::label::Displayable;
 use cushy::widgets::tree::Tree;
 use cushy::window::PendingWindow;
+
+fn make_node_with_label_and_buttons(name: &'static str) -> impl MakeWidget {
+    name.into_label()
+        .and("Delete"
+            .into_button()
+            .on_click(|event|{
+                // FIXME here we want to do this, but we have neither the tree nor the key
+                //       tree.remove_node(key);
+            })
+            .make_widget()
+        )
+        .and("Add child"
+            .into_button()
+            .on_click(|event|{
+                // FIXME here we want to do this, but we have neither the tree nor the key
+                //       tree.insert_child(make_node_with_label_and_buttons("generated child"), Some(key));
+            })
+            .make_widget()
+        )
+        .and("Add sibling"
+            .into_button()
+            .on_click(|event|{
+                // FIXME here we want to do this, but we have neither the tree nor the key
+                //       tree.insert_after(make_node_with_label_and_buttons("generated child"), key);
+            })
+            .make_widget()
+        )
+        .into_columns()
+        .make_widget()
+}
 
 #[cushy::main]
 fn main(app: &mut App) -> cushy::Result {
@@ -9,13 +40,13 @@ fn main(app: &mut App) -> cushy::Result {
     let pending = PendingWindow::default();
   
     let mut tree: Tree = Tree::default();
-    let root_key = tree.insert_child("root".to_string(), None).unwrap();
-    let child_key_1 = tree.insert_child("child 1".to_string(), Some(&root_key)).unwrap();
-    let nested_child_key_1 = tree.insert_child("nested 1".to_string(), Some(&child_key_1)).unwrap();
-    let _nested_child_key_2 = tree.insert_after("nested 2".to_string(), &nested_child_key_1).unwrap();
-    let child_key_2 = tree.insert_child("child 2".to_string(), Some(&root_key)).unwrap();
-    let nested_child_key_3 = tree.insert_child("nested 3".to_string(), Some(&child_key_2)).unwrap();
-    let _nested_child_key_4 = tree.insert_after("nested 4".to_string(), &nested_child_key_3);
+    let root_key = tree.insert_child(make_node_with_label_and_buttons("root"), None).unwrap();
+    let child_key_1 = tree.insert_child(make_node_with_label_and_buttons("child 1"), Some(&root_key)).unwrap();
+    let nested_child_key_1 = tree.insert_child(make_node_with_label_and_buttons("nested 1"), Some(&child_key_1)).unwrap();
+    let _nested_child_key_2 = tree.insert_after(make_node_with_label_and_buttons("nested 2"), &nested_child_key_1).unwrap();
+    let child_key_2 = tree.insert_child(make_node_with_label_and_buttons("child 2"), Some(&root_key)).unwrap();
+    let nested_child_key_3 = tree.insert_child(make_node_with_label_and_buttons("nested 3"), Some(&child_key_2)).unwrap();
+    let _nested_child_key_4 = tree.insert_after(make_node_with_label_and_buttons("nested 4"), &nested_child_key_3);
 
     let elements = "content above".contain()
         .and(tree.contain())
