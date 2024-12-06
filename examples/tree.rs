c9f616c978a977c435767a45c704f0239408577f
Dominic Clifton <me@dominicclifton.name>
2024-12-06 16:27:39 +0100

    Tree widget - WIP 10 - A tree actually renders.
    * the expand/collapse button doesn't work yet.

--- examples/tree.rs
+++ examples/tree.rs
@@ -10,8 +10,12 @@ fn main(app: &mut App) -> cushy::Result {
   
     let mut tree: Tree = Tree::default();
     let root_key = tree.insert_child("root".to_string(), None).unwrap();
-    let child_key = tree.insert_child("child".to_string(), Some(&root_key)).unwrap();
-    let _nested_child_key = tree.insert_child("nested".to_string(), Some(&child_key));
+    let child_key_1 = tree.insert_child("child 1".to_string(), Some(&root_key)).unwrap();
+    let nested_child_key_1 = tree.insert_child("nested 1".to_string(), Some(&child_key_1)).unwrap();
+    let _nested_child_key_2 = tree.insert_after("nested 2".to_string(), &nested_child_key_1).unwrap();
+    let child_key_2 = tree.insert_child("child 2".to_string(), Some(&root_key)).unwrap();
+    let nested_child_key_3 = tree.insert_child("nested 3".to_string(), Some(&child_key_2)).unwrap();
+    let _nested_child_key_4 = tree.insert_after("nested 4".to_string(), &nested_child_key_3);
 
     let elements = "content above".contain()
         .and(tree.contain())
